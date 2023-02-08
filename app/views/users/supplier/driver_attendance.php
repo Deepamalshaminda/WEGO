b048104d19dca800592c591eb65ca417e9517efe
ParindiDewmini <paridew99@gmail.com>
2023-02-09 01:28:49 +0530

    docs

--- app/views/users/supplier/driver_attendance.php
+++ app/views/users/supplier/driver_attendance.php
@@ -1,6 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/addvehicle.css">
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/earnings.css">
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/dashboard.css">
 <title><?php echo SITENAME; ?></title>
 </head>
@@ -109,3 +109,45 @@
     </nav>
      
     <div class="home-content">
+    <div class="table-card"> 
+<h3>Driver Attendance - Month of February</h3>
+
+<table>
+  <tr>
+    <th>Driver ID</th>
+    <th>Driver Name</th>
+    <th>Attendace </th>
+  </tr>
+
+  <tr>
+    <th>Driver ID 023</th>
+    <th>Steve Perera</th>
+    <th>20</th>
+  </tr>
+  
+  <tr>
+    <th>Driver ID 078</th>
+    <th>Kevin De Silva</th>
+    <th>18</th>
+  </tr>
+
+  <tr>
+    <th>Driver ID 029</th>
+    <th>Paul Fernando</th>
+    <th>16</th>
+  </tr>
+
+  <tr>
+    <th>Driver ID 078</th>
+    <th>Henry Perera</th>
+    <th>19</th>
+  </tr>
+  
+  <tr>
+    <th>Driver ID 045</th>
+    <th>Peter Parker</th>
+    <th>17</th>
+  </tr>
+  
+  
+  

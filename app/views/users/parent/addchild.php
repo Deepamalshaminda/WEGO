f14edb222b84a2a4feb95fdff3885894d45788ec
Naduni99 <100164811+Naduni99@users.noreply.github.com>
2023-02-21 20:18:15 +0530

    second

--- app/views/users/parent/addchild.php
+++ app/views/users/parent/addchild.php
@@ -1,5 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/addchild.css">
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/dashboard.css">
   <title><?php echo SITENAME; ?></title>
 </head>
 <body>
@@ -11,80 +12,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>
-  <div class="sidebar">
-    <div class="logo-details">
-      <div class='bx bxl-c-plus-plus'></div>
-      <span class="logo_name">&nbsp; WeGo</span>
-    </div>
-      <ul class="nav-links">
-        <li>
-          <a href="#" class="active">
-            <div class='bx bx-grid-alt' ></div>
-            <span class="links_name">&nbsp; Dashboard</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-box' ></div>
-            <span class="links_name">&nbsp; Send requests</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-list-ul' ></div>
-            <span class="links_name">&nbsp; Make reservations</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-pie-chart-alt-2' ></div>
-            <span class="links_name">&nbsp; Payments</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-coin-stack' ></div>
-            <span class="links_name">&nbsp; Student attendence</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-book-alt' ></div>
-            <span class="links_name">&nbsp; My added children</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-user' ></div>
-            <span class="links_name">&nbsp; </span>
-          </a>
-        </li>
-        
-        <li class="log_out">
-          <a href="#">
-            <div class='bx bx-log-out'></div>
-            <span class="links_name">&nbsp; Log out</span>
-          </a>
-        </li>
-      </ul>
-  </div>
-  <section class="home-section">
-    <nav>
-      <div class="sidebar-button">
-        <div class='bx bx-menu sidebarBtn'></div>
-        
-      </div>
-      <div class="search-box">
-        <input type="text" placeholder="Search...">
-        <div class='bx bx-search' ></div>
-      </div>
-      <div class="profile-details">
-        <!-- //<img src="../public/img/images.png" alt=""> -->
-        <span class="admin_name">Adam Fernando</span>
-        <div class='bx bx-chevron-down' ></div>
-      </div>
-    </nav>
-    </section>
+  <?php require APPROOT . '/views/inc/sidenavbar.php' ;?>
+    
 <!--<div class="full-page">
        <div class="register-wrapper">
               <div class="title-wrapper">
@@ -159,11 +88,13 @@
               </div>
               </form> 
        </div>-->
+       <h1 class="childtopic">Fill your child's details here..</h1>
+       <br>
        <section class="formback">
        <form action="<?php echo URLROOT; ?>/Parents/addchild" method="post">
       <div class="container">
-        <h1 class="topic">Fill your child's details here..</h1>
-        <p class="para">Please fill in this form to add your child's details.</p>
+        <h1 class="topic">Please fill this form with your child's details</h1>
+        
         <hr>
     
         <label for="email" class="fields"><b>Name*</b></label>
--- app/views/users/parent/sentrequest.php
+++ app/views/users/parent/sentrequest.php
@@ -13,76 +13,7 @@
 <body>
 
 
-<div class="sidebar">
-    <div class="logo-details">
-      <div class='bx bxl-c-plus-plus'></div>
-      <span class="logo_name">&nbsp; WeGo</span>
-    </div>
-      <ul class="nav-links">
-        <li>
-          <a href="parent.html" class="active">
-            <div class='bx bx-grid-alt' ></div>
-            <span class="links_name">&nbsp; Dashboard</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-box' ></div>
-            <span class="links_name">&nbsp; Send requests</span>
-          </a>
-        </li>
-        <li>
-          <a href="reserve.html">
-            <div class='bx bx-list-ul' ></div>
-            <span class="links_name">&nbsp; Make reservations</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-pie-chart-alt-2' ></div>
-            <span class="links_name">&nbsp; Payments</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-coin-stack' ></div>
-            <span class="links_name">&nbsp; Student attendence</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <div class='bx bx-book-alt' ></div>
-            <span class="links_name">&nbsp; My added children</span>
-          </a>
-        </li>
-        <li>
-          
-        
-        
-        <!--<li class="log_out">
-          <a href="#">
-            <div class='bx bx-log-out'></div>
-            <span class="links_name">&nbsp; Log out</span>
-          </a>
-        </li>-->
-      </ul>
-  </div>
-  <div class="home-section">
-    <nav>
-      <div class="sidebar-button">
-        <div class='bx bx-menu sidebarBtn'></div>
-        
-      </div>
-      <div class="search-box">
-        <input type="text" placeholder="Search...">
-        <div class='bx bx-search' ></div>
-      </div>
-      <div class="profile-details">
-        <!-- //<img src="../public/img/images.png" alt=""> -->
-        <span class="admin_name">Adam Fernando</span>
-        <div class='bx bx-chevron-down' ></div>
-      </div>
-    </nav>
+<?php require APPROOT . '/views/inc/sidenavbar.php' ;?>
      
     <div class="home-content">
 <button class="tablink" onclick="openPage('sendrequests')" id="defaultOpen" >Sent ride requests</button>

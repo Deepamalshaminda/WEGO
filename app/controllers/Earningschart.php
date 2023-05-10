cd1eafec33af9e6a7d75e9a2b2fcc8ec4b7238ff
ParindiDewmini <paridew99@gmail.com>
2023-05-10 23:02:20 +0530

    Earning_charts

--- app/controllers/Earningschart.php
+++ app/controllers/Earningschart.php
@@ -16,4 +16,32 @@ public function viewEarnings(){
             $this->view('users/supplier/earnings');
 }
 
+public function thisMonthIncome(){
+
+ // Get the current month and year
+ $current_month = date('m');
+ $current_year = date('Y');
+
+ // Get the total earnings for each vehicle for the current month
+ $earnings_model = $this->model('Earnings');
+ $earnings_data = $earnings_model->get_earnings_by_vehicle_and_month($current_month, $current_year);
+
+ // Pass the data to the view
+ $data = [
+     'earnings_data' => $earnings_data,
+ ];
+ $this->view('users/supplier/thisMonthIncome', $data);
 }
+}
+
+
+
+
+  //public function earningSummary(){
+
+//}
+
+//public function earningsFromVehicles(){
+
+//}
+

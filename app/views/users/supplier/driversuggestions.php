ca51399d5c0424645e0a9d359c3ab4c8387bd5c5
ParindiDewmini <paridew99@gmail.com>
2023-06-05 00:03:42 +0530

    driver suggestions

--- app/views/users/supplier/driversuggestions.php
+++ app/views/users/supplier/driversuggestions.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/dashboard.css">
-<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/prequest.css">
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/driversug.css">
 <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 <script type="text/javascript" src="<?php echo URLROOT; ?>/js/list.js"></script>
 
@@ -13,15 +13,19 @@
 
     <div class="home-content">
         <h1>Drivers Near You</h1>
-        <?php foreach ($drivers as $driver): ?>
-            <div class="driver-card">
-                <img src="<?php echo $driver['profile_image']; ?>">
-                <h3><?php echo $driver['name']; ?></h3>
-                <a href="driverdetails.php?id=<?php echo $driver['driver_id']; ?>">View Details</a>
-            </div>
-        <?php endforeach; ?>
+        <!-- <?php var_dump($drivers); ?>  -->
+        <?php foreach ($data['drivers'] as $driver): ?>
+  <div class="driver-card">
+    <img src="<?php echo $driver['profile_image']; ?>">
+   
+    <h3><?php echo $driver['name']; ?></h3>
+    <a href="driverdetails.php?id=<?php echo $driver['id']; ?>">View Details</a>
+  </div>
+<?php endforeach; ?>
+
     </div>
-    </body>
-</html>
+   
 <?php require APPROOT . '/views/inc/footer.php'; ?>
 
+
+   
